ba29e42b459a05bf144823cdb1455fc874431842
compandreimd <compandreimd@gmail.com>
2024-05-09 09:12:24 +0300

    Lesson2

--- src/vars/DemoVars.java
+++ src/vars/DemoVars.java
@@ -1,23 +1,20 @@
 package vars;
 
-import java.nio.charset.StandardCharsets;
-
 public class DemoVars {
     /**
      * boolean = true|false (1 bit)
-     * byte = 2^8 (-127) = 8 * bits
+     * byte = 2^8 (-128 ... 12) = 8 * bits
      * n
      */
     public String demo() {
         String name = "My name is Andrei. I am student QA!";
         System.out.println(name);
-
         System.out.println(name);
         System.out.println(name);
         System.out.println(name);
         return name;
-        // byte n = 113;
-        // 128| 64| 32| 16|  8|  4|  2|  1
-        //  0 | 1 | 1 | 1 | 0 | 0 | 0 |  1
+        // byte n = -15;
+        // -128| 64| 32| 16|  8|  4|  2|  1
+        //  1 | 1 | 1 | 1 | 0 | 0 | 0 |  1
     }
 }
