c52ee70ac6fd9679b8d8b91ecbc284250b640f5d
compandreimd <compandreimd@gmail.com>
2024-05-16 11:24:32 +0300

    Test

--- src/homework/HW1.java
+++ src/homework/HW1.java
@@ -9,19 +9,17 @@ public class HW1 {
         final double MAX;
         System.out.println("Introduceti maximum pe salariu?");
         MAX = scanner.nextDouble();
-        System.out.println("Introduceti salariele a 3 luni?");
-        double l1, l2, l3;
-        System.out.println("Prima luna?");
-        l1 = scanner.nextDouble();
-        System.out.println("A doua luna?");
-        l2 = scanner.nextDouble();
+        System.out.println("Introduceti salariele a 3 luni?"); int l1, l2, l3;
+        System.out.println("Prima luna?"); l1 = scanner.nextInt();
+        System.out.println("A doua luna?"); l2 = scanner.nextInt();
         System.out.println("A treia luna?");
-        l3 = scanner.nextDouble();
-        double sum = (l1 + l2 + l3);
+        l3 = scanner.nextInt();
+        long sum = (l1 + l2 + l3);
         System.out.println("Suma:" + sum);
         System.out.println("Media:" + (sum/3));
         System.out.println(ANSI_YELLOW+"Suma depasit de "+ (MAX)+ ": "+ (sum > MAX));
         System.out.println("Media depasita de "+ (MAX)+ ": "+ (sum/3 > MAX));
-        System.out.print(ANSI_RESET);
+        //No NEED System.out.print(ANSI_RESET);
+
     }
 }
