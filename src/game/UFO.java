636f813ea1262d09972b1e43727212b8d71e5163
compandreimd <compandreimd@gmail.com>
2024-05-30 09:25:19 +0300

    Add HW Game changes

--- src/game/UFO.java
+++ src/game/UFO.java
@@ -10,16 +10,17 @@ public class UFO {
     static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         //p,f,h,
-        System.out.println("Choose r(rock), s(scissors), p(paper)?");
+        System.out.print("Choose r(rock), s(scissors), p(paper)?");
         String my = scanner.nextLine();
         int i = 0;
-        while (!(my.equals("r")|| my.equals("s")|| my.equals("p"))){
-            System.out.println("Choose r(rock), s(scissors), p(paper)?");
-            my = scanner.nextLine();
-            if(++i >= 3){
-                System.out.println("Nu are rost!");
+        while (!(my.equals("r") || my.equals("s")|| my.equals("p"))){
+           if(++i >= 3){
+                System.out.println("There is no point!");
+                //(Exit app)
                 return;
             }
+            System.out.print("Choose r(rock), s(scissors), p(paper)?");
+            my = scanner.nextLine();
         }
         int pc = randomNumberGenerator.nextInt(0,100) % 3;
         if(pc == 0) //rock
