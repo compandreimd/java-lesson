2132445bd8b1b407d446b4aac244f6f78fe63369
Andrei <23dll95inmuzfetori8gfkwml642sj@bots.bitbucket.org>
2024-06-11 11:28:08 +0300

    triangle

--- src/forloop/ForLoop.java
+++ src/forloop/ForLoop.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 
 public class ForLoop {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 10;i++ ) {
-            System.out.println("Ce mai face?");
-            String res = scanner.nextLine();
-            if(!res.equals("bine")) break;
+        for (int i = 10; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
+//        for(int i = 0; i < 5; i++){
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print("*");
+//            }
+//            for (int j = 0; j < 5; j++) {
+//                System.out.print("_");
+//            }
+//            System.out.println();
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        for(int i = 0; i < 10;i++ ) {
+//            System.out.println("Ce mai face?");
+//            String res = scanner.nextLine();
+//            if(!res.equals("bine")) break;
+//        }
     }
 
 }
