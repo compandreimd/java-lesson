17474d08be692447954677fb3631fc622595fd65
Andrei <23dll95inmuzfetori8gfkwml642sj@bots.bitbucket.org>
2024-06-11 11:49:27 +0300

    triangles

--- src/forloop/ForLoop.java
+++ src/forloop/ForLoop.java
@@ -34,13 +34,46 @@ public class ForLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-
-        for (int i =size; i >= 0; i--) {
+        String line = "";
+        for (int i = 0; i < size; i++) {
+            line  += "-";
+        }
+        System.out.println(line);
+        for (int i =size; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+        System.out.println(line);
+        for (int i =size - 1; i >= 0; i--) {
+            for (int j = size - i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println(line);
+        for (int i =size; i > 0; i--) {
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println(line);
+        for (int i =size - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < size - i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        System.out.println(line);
 //        for(int i = 0; i < 5; i++){
 //            for (int j = 0; j < 5; j++) {
 //                System.out.print("*");
