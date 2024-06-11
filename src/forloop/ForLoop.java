0c328035296c1946c81e1e2dd5d95e6c6bc3061b
Andrei <23dll95inmuzfetori8gfkwml642sj@bots.bitbucket.org>
2024-06-11 10:09:37 +0300

    Changes fogot

--- src/forloop/ForLoop.java
+++ src/forloop/ForLoop.java
@@ -1,2 +1,15 @@
-package forloop;public class ForLoop {
+package forloop;
+
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < 10;i++ ) {
+            System.out.println("Ce mai face?");
+            String res = scanner.nextLine();
+            if(!res.equals("bine")) break;
+        }
+    }
+
 }
