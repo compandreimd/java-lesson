42ba4f28eaf84da0d249d240f8b3fd1b7c941666
Andrei <23dll95inmuzfetori8gfkwml642sj@bots.bitbucket.org>
2024-07-16 08:55:25 +0300

    HW6

--- src/homework/HW6.java
+++ src/homework/HW6.java
@@ -1,2 +1,36 @@
-package homework;public class HW6 {
+package homework;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class HW6 {
+
+    public static void main(String[] args) {
+        System.out.print("Get lines?:");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println("Have:");
+        var map = countChars(line);
+        for (var c : map.entrySet()) {
+            System.out.println("'"+c.getKey()+"':\t"+ c.getValue() +"||"+ Character.getName(c.getKey()));
+        }
+    }
+
+    public static @NotNull Map<Character, Integer> countChars(String str){
+        TreeMap<Character, Integer> rs = new TreeMap<>();
+        if(str == null)  return rs;
+        for(Character c : str.toUpperCase(Locale.ROOT).toCharArray()) {
+            if(rs.containsKey(c)){
+                rs.put(c, rs.get(c)+ 1);
+            }
+            else {
+                rs.put(c, 1);
+            }
+        }
+        return rs;
+    }
 }
